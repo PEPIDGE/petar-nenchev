a49651e1ed2c62d3c014a48a91faa71916041c56
PEPIDGE <pepkatabrat123@gmail.com>
2024-07-04 12:18:17 +0300

    updated content

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -3,38 +3,68 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
+import { FaTiktok } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 
 function ProjectCards(props) {
+  const renderButtonContent = (platform) => {
+    switch (platform) {
+      case "github":
+        return (
+          <>
+            <BsGithub /> GitHub
+          </>
+        );
+      case "tiktok":
+        return (
+          <>
+            <FaTiktok /> TikTok
+          </>
+        );
+      case "youtube":
+        return (
+          <>
+            <FaYoutube /> YouTube
+          </>
+        );
+      case "website":
+      default:
+        return (
+          <>
+            <CgWebsite /> Website
+          </>
+        );
+    }
+  };
+
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ opacity: 1 }} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
+        {props.link1 && (
+          <Button variant="primary" href={props.link1.url} target="_blank">
+            {renderButtonContent(props.link1.platform)}
+          </Button>
+        )}
         {"\n"}
         {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
+        {props.link2 && (
           <Button
             variant="primary"
-            href={props.demoLink}
+            href={props.link2.url}
             target="_blank"
             style={{ marginLeft: "10px" }}
           >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+            {renderButtonContent(props.link2.platform)}
           </Button>
         )}
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,11 +3,11 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
+import influencemediabg from "../../Assets/Projects/influencemediabg.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import bgdronefootage from "../../Assets/Projects/bgdronefootage.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import miami from "../../Assets/Projects/miami.png";
 
 function Projects() {
   return (
@@ -23,68 +23,63 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              imgPath={bgdronefootage}
+              title="Bg Drone Footage"
+              description="This is my personal website for drone services. I built this in 2022 and it was my first big project. I built it using React.js."
+              link1={{ url: "https://github.com/PEPIDGE/bg-drone-footage", platform: "github" }}
+              link2={{ url: "https://bgdronefootage.com/", platform: "website" }}
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              imgPath={miami}
+              title="Miami music video"
+              description="This is my biggest project that I have been involved in. It is a music video. I helped with all the sets and also did the drone shots."
+              link1={{ url: "https://www.youtube.com/watch?v=glbm4gdqT60", platform: "youtube" }}
+              link2={{ url: "https://miamimusicvideo.com/", platform: "website" }}
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              imgPath={influencemediabg}
+              title="Influence Media Bg"
+              description="This is my social media marketing agency TikTok. I am the videographer, manager, editor, and producer."
+              link1={{ url: "https://www.tiktok.com/@influence_media_bg", platform: "tiktok" }}
+              link2={{ url: "https://influencemediabg.com/", platform: "website" }}
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={leaf}
-              isBlog={false}
+              isBlog={"github"}
               title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              description="Used the plant disease dataset from Kaggle and trained an image classifier model using the 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using the Resnet34 pretrained model."
+              link1={{ url: "https://github.com/soumyajit4419/Plant_AI", platform: "github" }}
+              link2={{ url: "https://plant49-ai.herokuapp.com/", platform: "website" }}
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              isBlog={"github"}
+              title="AI For Social Good"
+              description="Using 'Natural Language Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace and thus helping in suicide prevention."
+              link1={{ url: "https://github.com/soumyajit4419/AI_For_Social_Good", platform: "github" }}
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              imgPath={editor}
+              isBlog={"github"}
+              title="Editor.io"
+              description="Online code and markdown editor built with React.js. Online Editor which supports HTML, CSS, and JS code with instant view of the website. Online markdown editor for building README files which supports GFM, Custom HTML tags with toolbar and instant preview. Both editors support auto-save of work using Local Storage."
+              link1={{ url: "https://github.com/soumyajit4419/Editor.io", platform: "github" }}
+              link2={{ url: "https://editor.soumya-jit.tech/", platform: "website" }}
             />
           </Col>
         </Row>
